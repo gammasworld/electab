7fa62b1e33d23651f041bd4e2ea2cd8316833529
Gammasworld <gtcortes89@gmail.com>
2018-06-26 23:23:17 -0700

    Gallery added

---
+++ js/custom.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function() {
+	
+	"use strict";
+	// Your custom js code goes here.
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+
+
+});
